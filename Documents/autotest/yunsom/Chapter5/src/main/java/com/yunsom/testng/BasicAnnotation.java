76b844bd5c8e6663b6c27f74a3f3b4f3ec16fb80
Mark.Liu <5020046@qq.com>
2020-04-20 11:37:17 +0800

    简单提交

---
+++ Documents/autotest/yunsom/Chapter5/src/main/java/com/yunsom/testng/BasicAnnotation.java
@@ -0,0 +1,47 @@
+package com.yunsom.testng;
+
+import org.testng.annotations.*;
+
+public class BasicAnnotation {
+
+    //最基本的注解，用来把方法标记为测试的一部分
+    @Test
+    public void testCase1(){
+        System.out.println("Test这是测试用例1");
+    }
+
+    @Test
+    public void testCase2() {
+        System.out.println("Test这是测试用例2");
+    }
+
+    @BeforeMethod
+    public void beforMethod(){
+        System.out.println("BeforMethod这是在测试方法之前运行的");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("AfterMethod这是在测试方法之后运行的");
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("BeforeClass这是在类之前运行的方法");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("AfterClass这是在类之后运行的方法");
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("BeforeSuite测试套件");
+    }
+
+    @AfterSuite
+    public void aftersuite(){
+        System.out.println("AfterSuite测试套件");
+    }
+}
